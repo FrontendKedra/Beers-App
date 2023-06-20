de696ebbb6d9338308226ea16eb94ed858bd10b1
Szymon Kedra <s5zee@wp.pl>
2023-06-20 20:49:17 +0200

    usePageNumber custom hook added

---
+++ src/common/customHooks/usePageNumber.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { UsePageNumber } from "../../interfaces/generalInterfaces";
+
+export const usePageNumber = (): UsePageNumber => {
+  const history = useHistory();
+  const location = useLocation();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(history.location.search);
+    const page = Number(searchParams.get("page")) || 1;
+    setPageNumber(page);
+  }, [history.location.search]);
+
+  const changePage = (newPage: number) => {
+    setPageNumber(newPage);
+    const searchParams = new URLSearchParams(history.location.search);
+    searchParams.set("page", newPage.toString());
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+
+  return [pageNumber, changePage];
+};
